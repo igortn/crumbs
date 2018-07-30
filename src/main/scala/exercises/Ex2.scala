b6d826aed8d26a6ce6fe7aca9138079a4fa66175
Igor Tovstopyat-Nelip <igor@sunlightpayments.com>
2018-07-29 22:50:12 -0400

    Implemented Ex2 with tests.

--- src/main/scala/exercises/Ex2.scala
+++ src/main/scala/exercises/Ex2.scala
@@ -1,6 +1,12 @@
 package exercises
 
+/*
+ * In the problem statement, the type of `notCombinableWith` is defined as `Seq[Promotion]`.
+ * This is an unnecessary complication, and it is probably an oversight because the sample data
+ * are given differently. Therefore, we are using `Seq[String]` here instead.
+ */
 case class Promotion(code: String, notCombinableWith: Seq[String])
+
 case class PromotionCombo(promotions: Seq[String])
 
 /*
@@ -8,7 +14,66 @@ case class PromotionCombo(promotions: Seq[String])
  */
 object Ex2 {
 
+/*
+ * There are multiple ways to implement this algorithm, but there are two different conceptual
+ * approaches that are dual to each other. The first approach would build all combinable promotions
+ * for the given code "bottom-up", i.e. starting with the code and sequentially adding all possible
+ * mutually allowed codes to grow the maximal combinable subsets. The second, "top-down" approach
+ * starts with the full set and sequentially applies the exclusion rules to split the original set
+ * into multiple allowed subsets. The "top-down" approach is simpler logically, and it is likely
+ * more performant. Another advantage of the "top-down" approach is that we can start with
+ * building the total set of maximal compatible subsets, and then this set can be reused to find
+ * combinable promotions for any promotion code. Therefore, we choose to use the "top-down"
+ * approach in this implementation.
+ */
+
+  /**
+   * Applies an exclusion rule to one internally compatible subset. The result is the sequence
+   * that contains either only initial subset, or two new internally compatible subsets.
+   */
+  private def splitSetWithRule(set: Set[String], rule: (String, Seq[String])): Seq[Set[String]] =
+    if (set.contains(rule._1) && set.exists(code => rule._2.contains(code))) {
+      val s1 = set - rule._1
+      val s2 = set -- rule._2
+      Seq(s1, s2)
+    } else {
+      Seq(set)
+    }
+
+  /**
+    * Applies an exclusion rule to a sequence of internally compatible subsets by splitting
+    * them as necessary.
+    */
+  def applyRule(subsets: Seq[Set[String]], rule: (String, Seq[String])): Seq[Set[String]] =
+    subsets.foldLeft(Seq.empty[Set[String]]) {
+      (acc, set) => acc ++ splitSetWithRule(set, rule)
+    }
+
+  /**
+    * Builds the sequence of all maximal internally compatible subsets for a given
+    * collection of promotions. The result of this method can be used to find combinable
+    * promotions for any promotion code. This can lead to a substantial optimization for
+    * large input sets. Another possible optimization can be based on the fact that the set
+    * of exclusion rules is inherently symmetric which could allow to cut down it size
+    * approximately in half. This was not pursued in the current implementation.
+    */
+  def allCombinableSubsets(allPromotions: Seq[Promotion]): Seq[Set[String]] = {
+    val exclusionRules: Seq[(String, Seq[String])] =
+      allPromotions.map { p => Promotion.unapply(p).get }
+    val initial: Seq[Set[String]] = Seq(allPromotions.map(_.code).toSet)
+
+    exclusionRules.foldLeft(initial) {
+      (subsets, rule) => applyRule(subsets, rule)
+    }
+  }
+
+  /**
+    * Finds all combinable promotions for a given promotion code.
+    */
   def combinablePromotions(promotionCode: String,
-                           allPromotions: Seq[Promotion]): Seq[PromotionCombo] = Seq()
+                           allPromotions: Seq[Promotion]): Seq[PromotionCombo] =
+    allCombinableSubsets(allPromotions)
+      .filter(_.contains(promotionCode))
+      .map { subset => PromotionCombo(subset.toSeq) }
 
 }
--- src/test/scala/exercises/Ex2Test.scala
+++ src/test/scala/exercises/Ex2Test.scala
@@ -6,6 +6,18 @@ import org.scalatest.FunSuite
 
 class Ex2Test extends FunSuite {
 
+  test("Apply an exclusion rule to a sequence of internally compatible subsets.") {
+    val subsets = Seq(Set("P1", "P2", "P4", "P5"), Set("P2", "P3", "P4", "P5"))
+    val rule1 = ("P2", Seq("P6"))
+    val rule2 = ("P6", Seq("P1", "P2"))
+    val rule3 = ("P2", Seq("P4", "P5"))
+
+    assert(applyRule(subsets, rule1) === subsets)
+    assert(applyRule(subsets, rule2) === subsets)
+    assert(applyRule(subsets, rule3) === Seq(
+      Set("P1", "P4", "P5"), Set("P1", "P2"), Set("P3", "P4", "P5"), Set("P2", "P3")))
+  }
+
   val promotions: Seq[Promotion] = Seq(
     Promotion("P1", Seq("P3")),
     Promotion("P2", Seq("P4", "P5")),
@@ -14,19 +26,25 @@ class Ex2Test extends FunSuite {
     Promotion("P5", Seq("P2"))
   )
 
-
-  test("Combinable promotions calculated correctly.") {
-    assert(combinablePromotions("P1", promotions) ===
-      Seq(
-        PromotionCombo(Seq("P1", "P2")),
-        PromotionCombo(Seq("P1", "P4", "P5"))
-      )
+  test("The full set of combinable subsets is built correctly.") {
+    assert(
+      allCombinableSubsets(promotions).toSet ===
+      Set(Set("P1","P2"), Set("P2","P3"), Set("P1","P4","P5"), Set("P3","P4","P5"))
     )
-    assert(combinablePromotions("P3", promotions) ===
-      Seq(
-        PromotionCombo(Seq("P3", "P2")),
-        PromotionCombo(Seq("P3", "P4", "P5"))
-      )
+  }
+
+  /*
+   * This tests the data set presented in the problem statement.
+   */
+  test("Combinable promotions calculated correctly.") {
+    assert(
+      combinablePromotions("P1", promotions).map(_.promotions.toSet).toSet ===
+        Set(Set("P1", "P2"), Set("P1", "P4", "P5"))
+    )
+
+    assert(
+      combinablePromotions("P3", promotions).map(_.promotions.toSet).toSet ===
+        Set(Set("P3", "P2"), Set("P3", "P4", "P5"))
     )
   }
 }
